e717d5edf9d7e84242beee977fa38ed644790bb4
Allen Ye <allenye0119@gmail.com>
2018-02-03 18:26:15 +0800

    Remove bold text

--- lib/section.zsh
+++ lib/section.zsh
@@ -14,22 +14,22 @@ spaceship::section() {
 
   [[ -z $3 && -z $4 ]] && content=$2 prefix=''
 
-  echo -n "%{%B%}" # set bold
+  #echo -n "%{%B%}" # set bold
   if [[ $spaceship_prompt_opened == true ]] && [[ $SPACESHIP_PROMPT_PREFIXES_SHOW == true ]]; then
     echo -n "$prefix"
   fi
   spaceship_prompt_opened=true
-  echo -n "%{%b%}" # unset bold
+  #echo -n "%{%b%}" # unset bold
 
-  echo -n "%{%B$color%}" # set color
+  echo -n "%{$color%}" # set color
   echo -n "$content"     # section content
-  echo -n "%{%b%f%}"     # unset color
+  echo -n "%{%f%}"     # unset color
 
-  echo -n "%{%B%}" # reset bold, if it was diabled before
+  #echo -n "%{%B%}" # reset bold, if it was diabled before
   if [[ $SPACESHIP_PROMPT_SUFFIXES_SHOW == true ]]; then
     echo -n "$suffix"
   fi
-  echo -n "%{%b%}" # unset bold
+  #echo -n "%{%b%}" # unset bold
 }
 
 # Compose whole prompt from sections
